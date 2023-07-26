6b0a6b7c24d22f5ecc7ba1b730687cc5ed421213
Tufan Chandra Pandu <tufan4344@gmail.com>
2023-07-27 01:06:45 +0530

    Add new folder Mini Project

---
+++ MERN GitHub/JavaScript/Mini_Project/PasswordValidator.js
@@ -0,0 +1,12 @@
+// Store Password
+const password = "15*Apr*1998"
+
+// Check the condition passward  is match or not
+let inputPwd = "15*Apr*1999";
+if(inputPwd === password){
+    console.log("Password Mathched, Password validation Successful.");
+}else{
+    console.log("Password didn't match. Password validation unsuccessful.")
+}
+
+
