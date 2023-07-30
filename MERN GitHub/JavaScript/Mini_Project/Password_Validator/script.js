97aadde5485c3859ff0d50d08ebc5749876a02c9
Tufan Chandra Pandu <tufan4344@gmail.com>
2023-07-30 16:58:44 +0530

    Make changes in Password validation script.js file

--- MERN GitHub/JavaScript/Mini_Project/Password_Validator/script.js
+++ MERN GitHub/JavaScript/Mini_Project/Password_Validator/script.js
@@ -1,21 +1,34 @@
-//Catch the elements 
-const inputPwd = document.getElementById("pwd");
-const inputMatchPwd = document.getElementById("matchPwd") ;
+//catch the tags
+
 const showStatus = document.getElementById("showStatus");
 const submit = document.getElementById("submit");
 
 
+/// Write Match function
+function matchPwd(){
 
+   const inputPwd = document.getElementById("pwd").value.trim();
+const inputMatchPwd = document.getElementById("matchPwd").value.trim();
+   // Check the box is empty or not 
+   //If box is not empty
+   if(inputPwd != 0 && inputMatchPwd != 0){
+      //Check   both box have the value is strictly equal 
+      if ( inputPwd === inputMatchPwd){
+         showStatus.innerText = "Password Matched. Password validation Successful."
+         showStatus.style.color = " #08ec14eb"
+      }else {
+         // for not equal 
+         showStatus.innerText = "Password didn't match. Password validation unsuccessful"
+         showStatus.style.color = "#ff0000"
 
-// Check the condition passward  is match or not
- function matchPwd(){
-    if(inputPwd.value === inputMatchPwd.value){
-       showStatus.innerText = "Password Matched, Password validation Successful";
-    }else{
-        showStatus.innerText ="Password didn't match. Password validation unsuccessful.";
-    }
- }
-submit.addEventListener("click", matchPwd() );
+      }
+   }else{
+      //Blank space
+      showStatus.innerText = "Please don't leave Empty box."
+      showStatus.style.color = "#00ffff"
+   }
+}
 
+submit.addEventListener("click" , matchPwd)
 
 
