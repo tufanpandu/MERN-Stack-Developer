2cdfd737943538655c6cd2da62fb7a5e533e4f90
Tufan Chandra Pandu <tufan4344@gmail.com>
2023-08-05 00:06:55 +0530

    add Error Handling fiel Try , Catch and finally

---
+++ MERN GitHub/JavaScript/Error Handling/tryCatch.js
@@ -0,0 +1,16 @@
+/// Try ,Catch and finally
+//It's helps for exceptional handling.
+//Try is for tested the code 
+// Catch is for excecute the code
+//Finally is print no matter Error is have or not .
+
+try{
+    console.log("Tufan") // it's not show any error
+    console.log(middle) // I am not define any variabel show it's show error . Now go to the catch block
+    console.log("Pandu") // it's not excitue . 
+}catch{
+    console.log("Chandra")
+    console.log("Tufan Chandra pandu")
+}finally{
+    console.log("My name is Tufan Chandra Pandu");//
+}
