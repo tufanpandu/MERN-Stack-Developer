efcf3cf6f2ac227d6669d62f03c13aef78aa3e50
tufanpandu <tufan4344@gmail.com>
2023-08-28 01:04:53 +0530

    Assignment add

--- JavaScript/Async & Await Fetch API/fetchAPI.js
+++ JavaScript/Async & Await Fetch API/fetchAPI.js
@@ -2,6 +2,9 @@
  * ✅- fetch() API is use in morder javascript for network requesting.
  * ✅- befor fetch API developer used  XMLHttpRequest method.
  * ✅- fetch api return new promise
+ * ✅-.json(): A method that returns a Promise resolving to the JSON representation of the response body. It's used when the response contains JSON data.
+ * ✅-.ok: A boolean property indicating whether the response status is within the range of 200 to 299, indicating success.
+ * ✅-.status: The HTTP status code of the response (e.g., 200 for OK, 404 for Not Found).
  */
 fetch('https://randomuser.me/api/').then((response)=>{
       if(!response.ok){
