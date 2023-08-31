cc0c0040ba13f0420e56aa3962fbe2c9e14d68e1
tufanpandu <tufan4344@gmail.com>
2023-09-01 00:26:37 +0530

    Highest Mark finding Assignment

--- ASSIGNMENTS/JavaScript Assignment/Assignment-1/highestMark.js
+++ ASSIGNMENTS/JavaScript Assignment/Assignment-1/highestMark.js
@@ -4,6 +4,14 @@ const mark = [42 ,50 , 95 , 35 , 55]
 //Ternary Operator
 // Approch -1
 let highestMark =  Math.max(...mark);
-console.log(highestMark);
+console.log(" Highest mark -1" ,highestMark);
 
 //approch -2 
+//Array of Student marks
+const marks = [85, 40 , 66,98 , 30]
+let highestMarkOne = 0;
+for(let i =0 ;i< marks.length;i++){
+    highestMarkOne = marks[i] > highestMarkOne ? marks[i] : highestMarkOne ;  
+}
+
+console.log("Highest Marks One is  " , highestMarkOne);
