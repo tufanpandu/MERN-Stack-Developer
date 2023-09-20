9ad6a5f55db4203988d0226352f09def0031551b
tufanpandu <tufan4344@gmail.com>
2023-09-21 00:11:07 +0530

    Add Second approch for move the imgae project

--- ASSIGNMENTS/JavaScript Assignment/Assignment-1/Move the Image/script.js
+++ ASSIGNMENTS/JavaScript Assignment/Assignment-1/Move the Image/script.js
@@ -1,36 +1,52 @@
 const img = document.querySelector("#wing");
+//Vertical 
+let positionY = 0;
+//Horizontal
+let positionX =0;
+//========= First approch =====================
 
-document.addEventListener("keyup" , function(event){
-    if(event.key !== "ArrowDown" && event.key !== "ArrowUp" && event.key !== "ArrowRight" && event.key !== "ArrowLeft"){
-       // Reset the positon if non-arrow key is released.
-        img.style.position = "unset";
-        console.log(event.key + "KeyUP")
-    }
-       
-})
 
 document.addEventListener("keydown" , function(event){
-    img.style.position = "absolute";
-    console.log(event);
+
     if(event.key === "ArrowDown"){
-        img.style.top = "unset";
-        img.style.bottom = "0";
-        img.style.left = "unset";
-        img.style.right = "unset";
+       positionY += 10;
     }else if(event.key === "ArrowUp"){
-        img.style.top = "0";
-        img.style.left = "auto";
-        img.style.right = "unset";
-        img.style.bottom = "unset";
+       positionY -= 10;
     }else  if(event.key === "ArrowRight"){
-        img.style.left = "unset";
-        img.style.bottom = "unset";
-        img.style.right = "0";
-        img.style.top = "auto";
+        positionX +=10;
     }else if(event.key === "ArrowLeft"){
-        img.style.right = "unset";
-        img.style.bottom = "unset";
-        img.style.left = "0";
-        img.style.top = "auto";
+        positionX -= 10;
+    }else{
+        return;
     }
+    img.style.transform = `translate(${positionX}px , ${positionY}px)`;
 })
+
+
+////================== Scecond approch =======================
+
+
+
+// document.addEventListener("keydown" , function(event){
+
+//     switch (event.key){
+//         case "ArrowUp":
+//            positionY -= 10;
+//            break;
+//         case "ArrowDown":
+//            positionY += 10;
+//            break; 
+//         case "ArrowLeft":
+//            positionX -= 10;
+//            break;   
+//         case "ArrowRight":
+//            positionX += 10;
+//            break; 
+//         default:
+//             return;   
+    
+//     }
+
+//     //update the position of image
+//     img.style.transform = `translate(${positionX}px , ${positionY}px)`;
+// })
