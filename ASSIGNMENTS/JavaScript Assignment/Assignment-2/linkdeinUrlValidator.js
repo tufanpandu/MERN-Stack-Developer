8a581153c3fefa531caa2789e542c2398d56e93a
tufanpandu <tufan4344@gmail.com>
2023-09-29 00:20:46 +0530

    Linkedin Profile URL Validator created using RegExp() method

---
+++ ASSIGNMENTS/JavaScript Assignment/Assignment-2/linkdeinUrlValidator.js
@@ -0,0 +1,13 @@
+//========= Linkden Profils URL validator ===================
+const inputUrl = "https://www.linkedin.com/in/tufanpandu/" 
+
+function linkedinUrlValidator(inputUrl){
+    const pattern = /^https?:\/\/(www\.)?linkedin\.com\/in\/[a-zA-Z0-9_-]{5,30}\/?$/;
+    if(pattern.test(inputUrl)){
+        console.log("Enter Linkedin profile URL is correct.");
+    }else{
+        console.log("Enter Valid URL");
+    }
+}
+
+linkedinUrlValidator(inputUrl);
