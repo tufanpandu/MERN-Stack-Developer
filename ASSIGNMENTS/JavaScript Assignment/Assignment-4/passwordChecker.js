a32261855ce67c0f171adb5856cf722f01115bcc
tufanpandu <tufan4344@gmail.com>
2023-10-11 00:30:03 +0530

    Password Checker prject is added using Getter and Setter

---
+++ ASSIGNMENTS/JavaScript Assignment/Assignment-4/passwordChecker.js
@@ -0,0 +1,63 @@
+// Password Checker
+//Approch -1
+class User{
+    constructor(userName,password){
+        this.userName = userName;
+        this.password = password;
+    }
+
+    get getPassword(){
+        return this.password.replace(/./g, '*') ;
+    }
+
+    
+    set setPassword(pw){
+        //Use Ragular Expression for check condions
+         let hasNumber = /\d/g.test(pw); // Check at last one number
+         let hasUpperChar = /[A-Z]/.test(pw); // chech at last one Upper Character
+        if(pw.length >= 8 && hasNumber && hasUpperChar ){
+            this.password = pw;
+        }else{
+            throw new Error("Invalid Password");
+        }
+    }
+}
+
+//Approch -2
+class UserOne {
+    constructor(userName, password){
+        this.userName = userName;
+        this.password = password;
+    }
+
+    get getPassword(){
+        return this.password.replace(/./g , '*');
+    }
+
+    set setPassword(pw){
+        let hasNumber = false;
+        let hasUpperChar = false;
+  for(let i =0;i<pw.length;i++){ // Iterate on PW 
+      let ch = pw.charAt(i);
+
+      if(ch === ch.toUpperCase()){ // check Upper Case
+        hasUpperChar = true;
+      }else if(!isNaN(pw)){ // Check Number
+        hasNumber = true;
+      }
+     
+  }
+  if(pw.length >= 8 && hasNumber && hasUpperChar){
+    this.password = pw;
+ }else{
+   console.log("Password most be at least 8 character long contain at least one number and one Upper letter");
+ }
+    }
+}
+
+const result = new User("Tufan" ,"Tufan*1998" );
+console.log(result.getPassword);
+
+console.log("Approch 2 is starting");
+const resultOne = new UserOne("Pondu" , "Tufan*1998");
+console.log(resultOne.getPassword);
